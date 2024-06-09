96a2aa434aefa63161acec06dd2f7a22f86f1823
Anastasia Nemenkina <gavrina.nastena@bk.ru>
2024-06-09 14:55:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 25000;
+        int Mile = TicketPrice / 20;
+        System.out.println("Кол-во начисленных миль" + Mile);
+    }
+}
